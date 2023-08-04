c4184f19a596c478267119ad005a3e32122c1c6f
Ravi Pandya <ravipandya@cmu.edu>
2023-08-04 14:20:10 -0400

    trained model on new data

--- nn_prob_pred.py
+++ nn_prob_pred.py
@@ -890,6 +890,6 @@ if __name__ == "__main__":
     # visualize_dataset(raw_data_path)
 
     # save new data and convert to h5 with featurization
-    raw_data_path = "./data/prob_pred/bayes_prob_branching.pkl"
-    processed_data_path = "./data/prob_pred/bayes_prob_branching_processed_feats.h5"
-    save_dataset_h5(raw_data_path, processed_data_path, n_init_cond=500, branching=True, n_traj=10, history=5, horizon=20)
+    raw_data_path = "./data/prob_pred/bayes_prob_branching_val.pkl"
+    processed_data_path = "./data/prob_pred/bayes_prob_branching_val_processed_feats.h5"
+    save_dataset_h5(raw_data_path, processed_data_path, n_init_cond=100, branching=True, n_traj=10, history=5, horizon=20)
